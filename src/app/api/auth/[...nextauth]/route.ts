f1d643064eb52fa289ca0b8b4b80235d8375a5f2
WilbertFE <wilbertbernadi699@gmail.com>
2025-12-19 21:05:18 +0700

    initial commit

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,38 @@
+import NextAuth from "next-auth";
+import { v4 as uuidv4 } from "uuid";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const handler = NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        const res = await fetch("/your/endpoint", {
+          method: "POST",
+          body: JSON.stringify(credentials),
+          headers: { "Content-Type": "application/json" },
+        });
+        const user = await res.json();
+        if (res.ok && user) {
+          return user;
+        }
+        return null;
+      },
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60,
+    updateAge: 24 * 60 * 60,
+    generateSessionToken: () => {
+      return uuidv4();
+    },
+  },
+  callbacks: {},
+});
+
+export { handler as GET, handler as POST };
