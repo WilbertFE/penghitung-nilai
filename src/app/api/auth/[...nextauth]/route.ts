9b2282c8430f3c8449359e6bb58fb850890ceb1d
WilbertFE <wilbertbernadi699@gmail.com>
2025-12-23 16:57:08 +0700

    feat: authentication feature done

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,38 +1,89 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import supabase from "@/lib/supabase";
 import NextAuth from "next-auth";
-import { v4 as uuidv4 } from "uuid";
 import CredentialsProvider from "next-auth/providers/credentials";
+import { v4 as uuidv4 } from "uuid";
 
 const handler = NextAuth({
+  session: {
+    strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60,
+    generateSessionToken: () => {
+      return uuidv4();
+    },
+  },
   providers: [
     CredentialsProvider({
       name: "Credentials",
       credentials: {
         username: { label: "Username", type: "text" },
+        email: { label: "Email", type: "email" },
         password: { label: "Password", type: "password" },
       },
-      async authorize(credentials, req) {
-        const res = await fetch("/your/endpoint", {
-          method: "POST",
-          body: JSON.stringify(credentials),
-          headers: { "Content-Type": "application/json" },
-        });
-        const user = await res.json();
-        if (res.ok && user) {
+      async authorize(credentials, req): Promise<any> {
+        console.log("credentials: ", credentials);
+
+        const user = {
+          uuid: uuidv4(),
+          username: credentials?.username,
+          email: credentials?.email,
+          password: credentials?.password,
+          role: "user",
+        };
+
+        if (user) {
+          console.log("success");
           return user;
+        } else {
+          return null;
         }
-        return null;
       },
     }),
   ],
-  session: {
-    strategy: "jwt",
-    maxAge: 30 * 24 * 60 * 60,
-    updateAge: 24 * 60 * 60,
-    generateSessionToken: () => {
-      return uuidv4();
+
+  callbacks: {
+    async jwt({ token, account, profile, user, trigger, session }) {
+      console.log("account : ", account);
+      console.log("profile : ", profile);
+      console.log("user : ", user);
+      console.log("trigger: ", trigger);
+
+      if (trigger === "signIn") {
+        if (account?.provider === "credentials") {
+          const { data, error } = await supabase
+            .from("users")
+            .insert({ ...user, type: "credentials" })
+            .select();
+          if (error) console.log(error);
+          else {
+            console.log("insert data");
+            console.log("data : ", data);
+            token.username = data[0].username;
+            token.role = data[0].role;
+          }
+        }
+      }
+      return token;
+    },
+    async session({ session, token }: any) {
+      if ("username" in token) {
+        session.user.username = token.username;
+      }
+
+      if ("role" in token) {
+        session.user.role = token.role;
+      }
+
+      if ("email" in token) {
+        session.user.email = token.email;
+      }
+
+      return session;
     },
   },
-  callbacks: {},
+  pages: {
+    signIn: "/signin",
+  },
 });
 
 export { handler as GET, handler as POST };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { signIn, useSession } from "next-auth/react";
+
 export default function Home() {
+  const { data: session, status } = useSession();
+
+  console.log("sessionn data : ", session);
+  console.log("status : ", status);
   return (
     <div>
-      <h1>Wilbert</h1>
+      <Button
+        variant="destructive"
+        type="button"
+        onClick={() => signIn("Credentials", { callbackUrl: "/" })}
+      >
+        Login
+      </Button>
     </div>
   );
 }
