3321601fab0c295d063a118b11b1e712fd005cdf
WilbertFE <wilbertbernadi699@gmail.com>
2025-12-23 20:21:04 +0700

    feat: hashing password

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -3,6 +3,7 @@ import supabase from "@/lib/supabase";
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { v4 as uuidv4 } from "uuid";
+import bcrypt from "bcryptjs";
 
 const handler = NextAuth({
   session: {
@@ -20,8 +21,8 @@ const handler = NextAuth({
         email: { label: "Email", type: "email" },
         password: { label: "Password", type: "password" },
       },
-      async authorize(credentials, req): Promise<any> {
-        console.log("credentials: ", credentials);
+      async authorize(credentials): Promise<any> {
+        // console.log("credentials: ", credentials);
 
         const user = {
           uuid: uuidv4(),
@@ -32,7 +33,7 @@ const handler = NextAuth({
         };
 
         if (user) {
-          console.log("success");
+          // console.log("success");
           return user;
         } else {
           return null;
@@ -42,22 +43,26 @@ const handler = NextAuth({
   ],
 
   callbacks: {
-    async jwt({ token, account, profile, user, trigger, session }) {
-      console.log("account : ", account);
-      console.log("profile : ", profile);
-      console.log("user : ", user);
-      console.log("trigger: ", trigger);
+    async jwt({ token, account, user, trigger }: any) {
+      // console.log("account : ", account);
+      // console.log("profile : ", profile);
+      // console.log("user : ", user);
+      // console.log("trigger: ", trigger);
 
       if (trigger === "signIn") {
         if (account?.provider === "credentials") {
+          const salt = await bcrypt.genSalt(10);
+          const hash = await bcrypt.hash(user.password, salt);
+
+          // console.log("hashed : ", hash);
           const { data, error } = await supabase
             .from("users")
-            .insert({ ...user, type: "credentials" })
+            .insert({ ...user, type: "credentials", password: hash })
             .select();
           if (error) console.log(error);
           else {
-            console.log("insert data");
-            console.log("data : ", data);
+            // console.log("insert data");
+            // console.log("data : ", data);
             token.username = data[0].username;
             token.role = data[0].role;
           }
